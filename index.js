9a2546d31ca01efc9da5a2bd3084ae3832c166a5
0xgeeky <techiebishwas@gmail.com>
2024-09-22 01:10:54 +0545

    Feat:Added code for websocket and file streams

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
 
@@ -16,10 +18,10 @@ const Blog = require("./models/blog");
 
 const app = express();
 
-const PORT = 8003;
+const PORT = process.env.PORT || 8000;
 
 // Connection mongodb
-connectMongoDb("mongodb://127.0.0.1:27017/blogging")
+connectMongoDb(process.env.MONGO_URL || "mongodb://127.0.0.1:27017/blogging")
   .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => console.log("Error", err));
 
@@ -48,3 +50,65 @@ app.use("/user", userRoute);
 app.use("/blog", blogRoute);
 
 app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
+
+/*
+// For websocket
+const express = require("express");
+const http = require("http");
+
+const app = express();
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const path = require("path");
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+  res.sendFile("/public/index.html");
+});
+
+// Socket io
+io.on("connection", (socket) => {
+  socket.on("user-message", (message) => {
+    io.emit("message", message);
+  });
+});
+
+server.listen(9000, () => {
+  console.log("Listening to port 9000");
+});
+
+*/
+
+/*
+// For file streams
+const express = require("express");
+const fs = require("fs");
+
+const status = require("express-status-monitor");
+
+const app = express();
+const PORT = 8002;
+
+app.use(status);
+
+// A program that takes the file and zipped it(reads file,make zip file in memory)
+// Stream Read-> 400 MB(zip) ->400MB
+
+fs.createReadStream("./sample.txt").pipe(
+  zlib.createGzip().pipe(fs.createWriteStream("./sample.zip"))
+);
+
+app.get("/", (req, res) => {
+  const stream = fs.createReadStream("./sample.txt", "utf-8");
+  stream.on("data", (chunk) => res.write(chunk));
+  stream.on("end", () => res.end());
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at ${PORT}`);
+});
+
+*/
